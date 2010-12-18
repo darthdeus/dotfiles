ac0c771b464d6c3ea9c796cc1f5cb7d1ee4a5601
Jakub Arnold <darthdeus@gmail.com>
2010-12-18 12:10:26 +0100

    added comments to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+echo "Installing required packages"
 ./script/install/required.sh
+echo "Linking dotfiles"
 ./script/install/link.sh
+echo "Installing vcprompt"
 ./script/install/vcprompt.sh
+echo "Installing rvm"
 ./script/install/rvm.sh
