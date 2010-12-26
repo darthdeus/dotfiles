6ddb9a3adbf1db37bfa3f51808cfaf98fd4fda65
Jakub Arnold <darthdeus@gmail.com>
2010-12-26 01:20:50 +0100

    added git config and ignore files

--- script/install/link.sh
+++ script/install/link.sh
@@ -2,3 +2,5 @@
 ln -nsf ~/bin/dotfiles/bashrc ~/.bashrc
 ln -nsf ~/bin/dotfiles/gemrc ~/.gemrc
 ln -nsf ~/bin/dotfiles/irbrc ~/.irbrc
+ln -nsf ~/bin/dotfiles/git/config ~/.gitconfig
+ln -nsf ~/bin/dotfiles/git/ignore ~/.gitignore
