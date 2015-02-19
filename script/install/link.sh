ac5c3e8114b4e4ef5a0aa5428294d23c968e60b1
Jakub Arnold <darthdeus@gmail.com>
2015-02-19 10:23:36 +0100

    Stop linking unused and outdated dotfiles

--- script/install/link.sh
+++ script/install/link.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 ln -nsf ~/bin/dotfiles/bashrc ~/.bashrc
-ln -nsf ~/bin/dotfiles/zshrc ~/.zshrc
 ln -nsf ~/bin/dotfiles/gemrc ~/.gemrc
 ln -nsf ~/bin/dotfiles/irbrc ~/.irbrc
-ln -nsf ~/bin/dotfiles/railsrc ~/.railsrc
 ln -nsf ~/bin/dotfiles/git/config ~/.gitconfig
 ln -nsf ~/bin/dotfiles/git/ignore ~/.gitignore
 ln -nsf ~/bin/dotfiles/tmux.conf ~/.tmux.conf
