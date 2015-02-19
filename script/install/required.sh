b67045519caab0002b95f6cacd7729bbee138eb6
Jakub Arnold <darthdeus@gmail.com>
2015-02-19 10:25:52 +0100

    Remove install required.sh

--- script/install/required.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# TODO - add condition for Ubuntu
-sudo apt-get install build-essential git-core mercurial zlib1g-dev libssl-dev libreadline6-dev libxml2-dev libsqlite3-dev curl vim
-
-# TODO - add homebrew installation here
