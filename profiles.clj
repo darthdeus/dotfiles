26eadd2e6cac1448e290a6d825d5492fcc1a3c0e
Jakub Arnold <darthdeus@gmail.com>
2015-04-09 23:56:53 +0200

    Add profiles.clj to dotfiles

---
+++ profiles.clj
@@ -0,0 +1,4 @@
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [lein-catnip "0.5.1" :exclusions [org.clojure/clojure]]
+                  [catnip "0.5.1" :exclusions [org.clojure/clojure]]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
