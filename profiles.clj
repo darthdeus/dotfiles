f711a7120298bd10e9ca185b8131d8e113432a00
Jakub Arnold <darthdeus@gmail.com>
2015-04-25 01:01:45 +0200

    Remove catnip from deps

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,2 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-catnip "0.5.1" :exclusions [org.clojure/clojure]]
-                  [catnip "0.5.1" :exclusions [org.clojure/clojure]]]
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"] ]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
