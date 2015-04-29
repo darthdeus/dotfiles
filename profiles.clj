e3d8b30e8249d4b290313c6b49bd5a7f471b6250
Jakub Arnold <darthdeus@gmail.com>
2015-04-29 07:26:46 +0200

    Add lein-try

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"] ]
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [lein-try "0.4.3"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
