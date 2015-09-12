b437aad29a72b34aee871e3791a60531f8ef7eaf
Jakub Arnold <darthdeus@gmail.com>
2015-09-12 15:15:14 +0000

    Update link.sh to link all the rc files

--- link.sh
+++ link.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-ln -nsf $HOME/.dotfiles/vim         $HOME/.vim
-ln -nsf $HOME/.dotfiles/vim/vimrc   $HOME/.vimrc
+DOT="$HOME/.dotfiles"
+
+ln -nsf "$DOT/vim" "$HOME/.vim"
+ln -nsf "$DOT/vim/vimrc" "$HOME/.vimrc"
+
+mkdir -p ~/.ssh
+ln -nsf "$DOT/ssh/config" "$HOME/.ssh/config"
+
+for file in "ackrc gemrc irbrc ghci tmux.conf"; do
+  ln -nsf "$DOT/$file" "$HOME/.$file"
+done
 
