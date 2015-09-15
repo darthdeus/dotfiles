9a2212f716e2902a409122ee89e0207e852f60bc
Jakub Arnold <darthdeus@gmail.com>
2015-09-15 03:18:28 +0000

    Fix link.sh

--- link.sh
+++ link.sh
@@ -13,7 +13,7 @@ ln -nsf "$DOT/emacs.d" "$HOME/.emacs.d"
 mkdir -p ~/.ssh
 ln -nsf "$DOT/ssh/config" "$HOME/.ssh/config"
 
-for file in "ackrc gemrc irbrc ghci tmux.conf"; do
+for file in ackrc gemrc irbrc ghci tmux.conf; do
   ln -nsf "$DOT/$file" "$HOME/.$file"
 done
 
