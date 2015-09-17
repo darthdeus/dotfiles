33f575b093b199bd385bfc97521d3202025d3171
Jakub Arnold <darthdeus@gmail.com>
2015-09-17 16:33:39 +0200

    Add base16 color settings for xfce4 terminal

--- link.sh
+++ link.sh
@@ -10,10 +10,13 @@ ln -nsf "$DOT/zsh/zshrc" "$HOME/.zshrc"
 
 ln -nsf "$DOT/emacs.d" "$HOME/.emacs.d"
 
-mkdir -p ~/.ssh
+mkdir -p "$HOME/.ssh"
 ln -nsf "$DOT/ssh/config" "$HOME/.ssh/config"
 
 for file in ackrc gemrc irbrc ghci tmux.conf; do
   ln -nsf "$DOT/$file" "$HOME/.$file"
 done
 
+XFCE_TERMRC_PATH="$HOME/.config/xfce4/terminal/"
+mkdir -p "$XFCE_TERMRC_PATH"
+ln -nsf "$HOME/.dotfiles/base16-default.dark.terminalrc" "$XFCE_TERMRC_PATH/terminalrc"
