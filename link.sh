337daa5384b991c2a2fcdd4cd1c9a5f7b29fa0a4
Jakub Arnold <darthdeus@gmail.com>
2015-09-25 14:36:57 +0000

    Cleanup link.sh

--- link.sh
+++ link.sh
@@ -2,18 +2,18 @@
 
 DOT="$HOME/.dotfiles"
 
-ln -nsf "$DOT/vim" "$HOME/.vim"
-ln -nsf "$DOT/vim/vimrc" "$HOME/.vimrc"
-
-ln -nsf "$DOT/zsh" "$HOME/.zsh"
-ln -nsf "$DOT/zsh/zshrc" "$HOME/.zshrc"
-
-ln -nsf "$DOT/emacs.d" "$HOME/.emacs.d"
+ln -nsf "$DOT/vim"        "$HOME/.vim"
+ln -nsf "$DOT/vim/vimrc"  "$HOME/.vimrc"
+ln -nsf "$DOT/zsh"        "$HOME/.zsh"
+ln -nsf "$DOT/zsh/zshrc"  "$HOME/.zshrc"
+ln -nsf "$DOT/emacs.d"    "$HOME/.emacs.d"
+ln -nsf "$DOT/xmonad"     "$HOME/.xmonad"
+ln -nsf "$DOT/Xresources" "$HOME/.Xresources"
 
 mkdir -p "$HOME/.ssh"
 ln -nsf "$DOT/ssh/config" "$HOME/.ssh/config"
 
-for file in ackrc gemrc irbrc ghci tmux.conf; do
+for file in ackrc gemrc irbrc ghci tmux.conf ycm_extra_conf.py; do
   ln -nsf "$DOT/$file" "$HOME/.$file"
 done
 
