5f9cf96a6ddafe87d216280aa266cf59fcdf1671
Jakub Arnold <darthdeus@gmail.com>
2015-10-13 09:42:17 +0200

    Minor cleanup

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,5 +1,6 @@
 import XMonad
 
+-- TODO - automatically detect which version of st should be run
 main = xmonad defaultConfig
         { terminal    = "~/bin/st -e tmux"
         , modMask     = mod4Mask
