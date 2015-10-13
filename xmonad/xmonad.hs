5ecf4fe1b918ae6137cca7e130caf6b01ef1c161
Jakub Arnold <darthdeus@gmail.com>
2015-10-13 09:41:45 +0200

    Configure xmonad to launch my own copy of st

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,7 @@
 import XMonad
 
 main = xmonad defaultConfig
-        { terminal    = "st -e tmux"
+        { terminal    = "~/bin/st -e tmux"
         , modMask     = mod4Mask
         , borderWidth = 3
         }
