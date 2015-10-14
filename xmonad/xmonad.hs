9eae3a16d3f3bd093507dd1d0b44a506043a6391
Jakub Arnold <darthdeus@gmail.com>
2015-10-15 01:30:18 +0200

    A lot of cleanup + experimental xmonad config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,8 +1,34 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.UrgencyHook
+import XMonad.Util.EZConfig
+import XMonad.Util.WorkspaceCompare
 
--- TODO - automatically detect which version of st should be run
-main = xmonad defaultConfig
+main :: IO ()
+main = do
+  conf <- statusBar "dzen2 -p -xs 1 -ta l"
+    dzenPP
+      { ppHidden = pad
+      , ppTitle = pad . dzenColor "#bbb" "" . dzenEscape
+      , ppLayout = const ""
+      , ppSort = getSortByXineramaRule
+      }
+    toggleStrutsKey
+    $ withUrgencyHook NoUrgencyHook
+    $ defaultConfig
         { terminal    = "~/.dotfiles/bin/st -e tmux"
         , modMask     = mod4Mask
-        , borderWidth = 3
+        , borderWidth = 2
         }
+
+  xmonad conf
+
+toggleStrutsKey XConfig { modMask = modm } = (modm, xK_b)
+
+-- TODO - automatically detect which version of st should be run
+-- main = xmonad defaultConfig
+--         { terminal    = "~/.dotfiles/bin/st -e tmux"
+--         , modMask     = mod4Mask
+--         , borderWidth = 2
+--         }
