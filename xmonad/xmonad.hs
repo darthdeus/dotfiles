31bad86b52241e31685bc686a9716bf578fa0ad3
Jakub Arnold <darthdeus@gmail.com>
2015-10-15 00:43:49 +0200

    Update xmonad.hs to use dotfiles' st build

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,7 +2,7 @@ import XMonad
 
 -- TODO - automatically detect which version of st should be run
 main = xmonad defaultConfig
-        { terminal    = "~/bin/st -e tmux"
+        { terminal    = "~/.dotfiles/bin/st -e tmux"
         , modMask     = mod4Mask
         , borderWidth = 3
         }
