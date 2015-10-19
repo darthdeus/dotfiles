7aacbef17614f5d3d7c052be46ffe261a05b72b7
Jakub Arnold <darthdeus@gmail.com>
2015-10-19 01:36:44 +0000

    Minor cleanup

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
+static char font[] = "Fantasque Sans Mono:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
