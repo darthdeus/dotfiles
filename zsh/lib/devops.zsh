1d574b209d0800675b16706f9e501653c5428419
Jakub Arnold <darthdeus@gmail.com>
2015-11-05 11:44:47 +0000

    Update devops-reload-virtlab to update cat as well

--- zsh/lib/devops.zsh
+++ zsh/lib/devops.zsh
@@ -1,5 +1,11 @@
 function devops-reload-virtlab() {
-  for server in mff-nix-virtlab-qemu-{a-noforward,b,bb,c,cc}; do
+  set -A servers mff-nix-virtlab-qemu-{a-noforward,b,bb,c,cc}
+
+  if [ "$1" = "cat" ]; then
+    servers+=( cat )
+  fi
+
+  for server in $servers; do
     echo "Reloading $server"
     ssh $server "cd .devops; git pull"
   done
