125c28c46ee98d6525e5f973d4db25aeddc95326
Jakub Arnold <darthdeus@gmail.com>
2017-01-13 12:30:06 +0100

    Fix st alias + pacman install script

--- deps.sh
+++ deps.sh
@@ -1,1 +1,7 @@
-pacman -S libxft base-devel tmux git openssh iw wpa_supplicant dialog
+#!/bin/sh
+pacman -S libxft base-devel tmux git openssh iw wpa_supplicant dialog dmenu firefox terminator
+
+# pacman-key --init
+# pacman-key --populate archlinux
+
+# gpg --recv-key KEY
