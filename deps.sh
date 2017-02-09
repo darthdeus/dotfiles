f968d6e7e614363c2bd22de411716e424771c7a2
Jakub Arnold <darthdeus@gmail.com>
2017-02-09 13:48:38 +0100

    Add netstat & lsof to deps

--- deps.sh
+++ deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pacman -S libxft base-devel tmux git openssh iw wpa_supplicant dialog dmenu firefox terminator hub htop mlocate
+pacman -S libxft base-devel tmux git openssh iw wpa_supplicant dialog dmenu firefox terminator hub htop mlocate net-tools lsof
 
 # pacman-key --init
 # pacman-key --populate archlinux
