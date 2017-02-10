f33aade977c456bc4f32b620f3d1f5865db2b602
Jakub Arnold <darthdeus@gmail.com>
2017-02-10 18:15:30 +0100

    Run deps install with sudo

--- deps.sh
+++ deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pacman -S --needed libxft base-devel tmux git openssh iw wpa_supplicant dialog dmenu firefox terminator hub htop mlocate net-tools lsof alsa-utils perf
+sudo pacman -S --needed libxft base-devel tmux git openssh iw wpa_supplicant dialog dmenu firefox terminator hub htop mlocate net-tools lsof alsa-utils perf
 
 # pacman-key --init
 # pacman-key --populate archlinux
