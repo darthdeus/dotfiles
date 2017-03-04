b4fcf2bc7e567f70fa40e7465815cf0cf83cc10b
Jakub Arnold <darthdeus@gmail.com>
2017-03-04 01:41:27 +0100

    Modify rbenv-install.sh to run git-pull if the dirs exist

--- rbenv-install.sh
+++ rbenv-install.sh
@@ -1,3 +1,19 @@
-#!/bin/sh
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+#!/bin/bash
+
+RBENV="$HOME/.rbenv"
+RUBY_BUILD="$RBENV/plugins/ruby-build"
+
+if [ -d "$RBENV" ]; then
+  cd ~/.rbenv
+  git pull
+
+  if [ -d "$RUBY_BUILD" ]; then
+    cd "$RUBY_BUILD"
+    git pull
+  else
+    git clone https://github.com/rbenv/ruby-build.git "$RUBY_BUILD"
+  fi
+else
+  git clone https://github.com/rbenv/rbenv.git "$RBENV"
+  git clone https://github.com/rbenv/ruby-build.git "$RUBY_BUILD"
+fi
