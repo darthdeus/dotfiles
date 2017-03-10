021b4e86274006860194d93da5d3fd9c6bbec7e6
Jakub Arnold <darthdeus@gmail.com>
2017-03-10 14:50:50 +0100

    Add mosh & abs to deps.sh

--- deps.sh
+++ deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo pacman -S --needed libxft tmux git openssh iw wpa_supplicant dialog dmenu firefox terminator hub htop mlocate net-tools lsof alsa-utils perf acpi cantarell-fonts wicd wicd-gtk xclip xsel termite clang clang-tools-extra openmp slock openmpi valgrind nodejs npm gdb cgdb pkgfile ranger lynx elinks sudo qutebrowser strace rsync
+sudo pacman -S --needed libxft tmux git openssh iw wpa_supplicant dialog dmenu firefox terminator hub htop mlocate net-tools lsof alsa-utils perf acpi cantarell-fonts wicd wicd-gtk xclip xsel termite clang clang-tools-extra openmp slock openmpi valgrind nodejs npm gdb cgdb pkgfile ranger lynx elinks sudo qutebrowser strace rsync mosh abs
 
 # pacman-key --init
 # pacman-key --populate archlinux
