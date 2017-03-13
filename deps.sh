5da89f7b2609d5c8c9ccbd8c7a0e0d5f1b3a9476
Jakub Arnold <darthdeus@gmail.com>
2017-03-13 17:03:07 +0100

    Add atop to deps.sh

--- deps.sh
+++ deps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # TODO: qutebrowser or AUR qutebrowser-git?
-sudo pacman -S --needed libxft tmux git openssh iw wpa_supplicant dialog dmenu firefox terminator hub htop mlocate net-tools lsof alsa-utils perf acpi cantarell-fonts wicd wicd-gtk xclip xsel termite clang clang-tools-extra openmp slock openmpi valgrind nodejs npm gdb cgdb pkgfile ranger lynx elinks sudo strace rsync mosh abs unzip ctags lldb cloc
+sudo pacman -S --needed libxft tmux git openssh iw wpa_supplicant dialog dmenu firefox terminator hub htop mlocate net-tools lsof alsa-utils perf acpi cantarell-fonts wicd wicd-gtk xclip xsel termite clang clang-tools-extra openmp slock openmpi valgrind nodejs npm gdb cgdb pkgfile ranger lynx elinks sudo strace rsync mosh abs unzip ctags lldb cloc atop
 
 # pacman-key --init
 # pacman-key --populate archlinux
