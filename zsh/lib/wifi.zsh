cfc70427f81414b34c76f492c32c61b56214eda2
Jakub Arnold <darthdeus@gmail.com>
2018-12-03 15:27:32 +0100

    Automatically activate mrk-proxy on Wireless1

---
+++ zsh/lib/wifi.zsh
@@ -0,0 +1,4 @@
+function get-ssid() {
+  # TODO: ubuntu
+  /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'
+}
