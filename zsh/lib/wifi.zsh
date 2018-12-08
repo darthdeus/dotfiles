e916018904973d68a2786bc96a35187585462509
Jakub Arnold <darthdeus@gmail.com>
2018-12-08 17:29:15 +0100

    Make get-ssid not crash on linux

--- zsh/lib/wifi.zsh
+++ zsh/lib/wifi.zsh
@@ -1,4 +1,7 @@
 function get-ssid() {
   # TODO: ubuntu
-  /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'
+  local WIFI_CMD=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport
+  if [ -f "$WIFI_CMD" ]; then
+    $WIFI_CMD -I | awk '/ SSID/ {print substr($0, index($0, $2))}'
+  fi
 }
