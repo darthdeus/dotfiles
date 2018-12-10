511c290d86eae8e46439b0fb373de47c9514719b
Jakub Arnold <darthdeus@gmail.com>
2018-12-10 01:06:35 +0100

    Add utils/build-vim.sh

---
+++ utils/build-vim.sh
@@ -0,0 +1,12 @@
+./configure --with-features=huge \
+            --enable-multibyte \
+	    --enable-rubyinterp=yes \
+	    --enable-pythoninterp=yes \
+	    --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu/ \
+	    --enable-python3interp=yes \
+	    --with-python3-config-dir=/usr/lib/python3.6/config-3.6m/x86_64-linux-gnu \
+	    --enable-perlinterp=yes \
+	    --enable-luainterp=yes \
+            --enable-gui=gtk2 \
+            --enable-cscope \
+            --prefix=~/.local
