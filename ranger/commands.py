0039eefbfa546f24b3f10031e664341d60e4055c
Jakub Arnold <darthdeus@gmail.com>
2018-12-11 23:08:47 +0100

    Use previews in ranger fzf

--- ranger/commands.py
+++ ranger/commands.py
@@ -15,12 +15,12 @@ class fzf_select(Command):
         import os.path
         if self.quantifier:
             # match only directories
-            command="fd -t d --hidden | fzf +m"
+            command="fd -t d --hidden | fzf +m --preview 'cat {}'"
             # command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
             # -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
         else:
             # match files and directories
-            command="fd --hidden | fzf +m"
+            command="fd --hidden | fzf +m --preview 'cat {}'"
             # command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
             # -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
         fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
