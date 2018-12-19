77fbcffe17bf5da6136340ebc7fe873597ce2355
Jakub Arnold <darthdeus@gmail.com>
2018-12-19 10:05:25 +0100

    Add ranger previews

--- ranger/commands.py
+++ ranger/commands.py
@@ -15,14 +15,15 @@ class fzf_select(Command):
         import os.path
         if self.quantifier:
             # match only directories
-            command="fd -t d --hidden | fzf +m --preview 'cat {}'"
+            command="fd -t d --hidden | fzf +m --preview '$([[ -d {} ]] && echo ls || echo cat) {}'"
             # command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
             # -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
         else:
             # match files and directories
-            command="fd --hidden | fzf +m --preview 'cat {}'"
+            command="fd --hidden | fzf +m --preview '$([[ -d {} ]] && echo ls || echo cat) {}'"
             # command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
             # -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+
         fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
