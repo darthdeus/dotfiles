b989dc111f5627ea7d5cae128ce9932bb4b6689e
Jakub Arnold <darthdeus@gmail.com>
2018-12-25 01:26:11 +0100

    Allow venv as well as .venv

--- zsh/lib/python.zsh
+++ zsh/lib/python.zsh
@@ -1,12 +1,16 @@
 function sa() {
   if [ -f .venv/bin/activate ]; then
     source .venv/bin/activate
+  elif [ -f venv/bin/activate ]; then
+    source venv/bin/activate
   fi
 }
 
 function vpip() {
   if [ -f .venv/bin/pip ]; then
     ./.venv/bin/pip $@
+  elif [ -f venv/bin/pip ]; then
+    ./venv/bin/pip $@
   else
     echo "virtualenv not found in .venv"
     exit 1
