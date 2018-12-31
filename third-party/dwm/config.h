842e62fa684727fa20e88a2404443f2ae0a43820
Jakub Arnold <darthdeus@gmail.com>
2018-12-31 03:14:34 +0100

    Try to add Discord to floating in dwm

--- third-party/dwm/config.h
+++ third-party/dwm/config.h
@@ -29,6 +29,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
+  { "Discord",  NULL,  "Discord",       1 << 8,       True,        -1 },
 };
 
 /* layout(s) */
