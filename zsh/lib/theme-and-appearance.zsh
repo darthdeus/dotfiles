f3b42a0c8039ceabafe33be232fe64b2a1c3407f
Jakub Arnold <darthdeus@gmail.com>
2019-01-10 21:33:47 +0100

    Disable AUTO_CD

--- zsh/lib/theme-and-appearance.zsh
+++ zsh/lib/theme-and-appearance.zsh
@@ -10,8 +10,8 @@ then
   ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
 fi
 
-#setopt no_beep
-setopt auto_cd
+# setopt no_beep
+# setopt auto_cd
 setopt multios
 setopt cdablevarS
 
