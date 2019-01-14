e059582338ec499a236012b31f7dbeedc90bc5d9
Jakub Arnold <darthdeus@gmail.com>
2019-01-14 14:46:03 +0100

    Unified neovim-deps

--- utils/neovim-deps.sh
+++ utils/neovim-deps.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 set -euxo pipefail
 
-mkdir -p ~/.venv/
+VENVS_DIR="$HOME/.venvs"
 
-python2 -m pip install virtualenv
-python3 -m pip install virtualenv
+mkdir -p "$VENVS_DIR"
 
-python2 -m virtualenv ~/.venv/neovim2
-~/.venv/neovim2/bin/pip install neovim
+virtualenv -p python2 "$VENVS_DIR/neovim2"
+"$VENVS_DIR/neovim2/bin/pip" install neovim
 
-python3 -m virtualenv ~/.venv/neovim3
-~/.venv/neovim3/bin/pip install neovim mypy flake8
+virtualenv -p python3 "$VENVS_DIR/neovim3"
+"$VENVS_DIR/neovim3/bin/pip" install neovim mypy flake8
+
+# python2 -m pip install virtualenv
+# python3 -m pip install virtualenv
+#
+# python2 -m virtualenv ~/.venv/neovim2
+# ~/.venv/neovim2/bin/pip install neovim
+#
+# python3 -m virtualenv ~/.venv/neovim3
+# ~/.venv/neovim3/bin/pip install neovim mypy flake8
 
 
 # The following is optional, and the neovim3 env is still active
