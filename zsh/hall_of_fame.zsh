172f18e0d095e566991a94ad13554f44dd6b26cb
Jakub Arnold <darthdeus@gmail.com>
2019-11-08 22:57:07 +0100

    Cleanup old macos stuff

--- zsh/hall_of_fame.zsh
+++ zsh/hall_of_fame.zsh
@@ -103,3 +103,10 @@ if [ "$(get-ssid)" = "Wireless1" ]; then
   mpo
 fi
 
+function get-ssid() {
+  # TODO: ubuntu
+  local WIFI_CMD=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport
+  if [ -f "$WIFI_CMD" ]; then
+    $WIFI_CMD -I | awk '/ SSID/ {print substr($0, index($0, $2))}'
+  fi
+}
--- zsh/lib/wifi.zsh
+++
@@ -1,7 +0,0 @@
-function get-ssid() {
-  # TODO: ubuntu
-  local WIFI_CMD=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport
-  if [ -f "$WIFI_CMD" ]; then
-    $WIFI_CMD -I | awk '/ SSID/ {print substr($0, index($0, $2))}'
-  fi
-}
