9dd65fea53c1aa399c39a223b0bcc666edf48346
Jakub Arnold <darthdeus@gmail.com>
2019-11-17 21:26:53 +0100

    Add nixpkgs config + symlink

--- utils/link.sh
+++ utils/link.sh
@@ -13,6 +13,8 @@ ln -nsf "$DOT/zsh/zshrc"     "$HOME/.zshrc"
 ln -nsf "$DOT/xinitrc"       "$HOME/.xinitrc"
 ln -nsf "$DOT/Xmodmap"       "$HOME/.Xmodmap"
 
+ln -nsf "$DOT/nixpkgs"        "$HOME/.config/nixpkgs"
+
 mkdir -p "$HOME/.newsboat"
 ln -nsf "$DOT/newsboat-urls" "$HOME/.newsboat/urls"
 
