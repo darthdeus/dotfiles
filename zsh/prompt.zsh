de78dae51c04f0ab6afdbbdbd90776e4fa85f129
Jakub Arnold <darthdeus@gmail.com>
2019-11-27 23:14:22 +0100

    Cleanup prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -9,8 +9,8 @@ function current_commit() {
 }
 
 function minutes_since_last_commit {
-    now=`date +%s`
-    last_commit=`git log --pretty=format:'%at' -1`
+    now=$(date +%s)
+    last_commit=$(git log --pretty=format:'%at' -1)
     seconds_since_last_commit=$((now-last_commit))
     minutes_since_last_commit=$((seconds_since_last_commit/60))
     echo $minutes_since_last_commit
