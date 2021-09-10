9e3df394d0f2139d82919cce41e6bd6a4f6b0f1c
Jakub Arnold <darthdeus@gmail.com>
2021-09-10 02:46:37 +0200

    Emacs tiny update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -442,8 +442,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (magit rainbow-mode ido-completing-read+ amx avy smex ido-vertical-mode org-bullets spacemacs-theme beacon base16-theme which-key use-package evil))))
+   '(magit rainbow-mode ido-completing-read+ amx avy smex ido-vertical-mode org-bullets spacemacs-theme beacon base16-theme which-key use-package evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
