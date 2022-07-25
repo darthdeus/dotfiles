fb6fd3cdc14fc6e53170478b2ef4f8002a00c5fe
Jakub Arnold <darthdeus@gmail.com>
2022-07-25 20:01:38 +0200

    Temporarily disable treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -90,10 +90,10 @@ require("packer").startup(function()
 	use("othree/html5.vim")
 	use("mattn/emmet-vim")
 
-	if vim.fn.has("win32") ~= 1 then
-		use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-		use("nvim-treesitter/playground")
-	end
+	-- if vim.fn.has("win32") ~= 1 then
+	-- 	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	-- 	use("nvim-treesitter/playground")
+	-- end
 	use("chaimleib/vim-renpy")
 
 	use("DingDean/wgsl.vim")
@@ -783,76 +783,76 @@ lspconfig.sumneko_lua.setup({
 -----------------------------------
 -----------------------------------
 
-if vim.fn.has("win32") ~= 1 then
-	require("nvim-treesitter.configs").setup({
-		ensure_installed = { "c", "cpp", "json", "javascript", "go", "python", "rust", "query", "lua" },
-		highlight = {
-			enable = true,
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				-- init_selection = "gnn",
-				-- node_incremental = "grn",
-				-- scope_incremental = "rc",
-				-- node_decremental = "grm",
-				init_selection = "`",
-				node_incremental = "`",
-				node_decremental = "~",
-				scope_incremental = "rc",
-			},
-		},
-		textobjects = {
-			select = {
-				enable = true,
-				keymaps = {
-					-- You can use the capture groups defined in textobjects.scm
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["ac"] = "@class.outer",
-					["ic"] = "@class.inner",
-
-					-- Or you can define your own textobjects like this
-					["iF"] = {
-						python = "(function_definition) @function",
-						cpp = "(function_definition) @function",
-						c = "(function_definition) @function",
-						java = "(method_declaration) @function",
-					},
-				},
-			},
-			move = {
-				enable = true,
-				goto_next_start = {
-					["]a"] = "@function.outer",
-					["]]"] = "@class.outer",
-				},
-				goto_next_end = {
-					["]A"] = "@function.outer",
-					["]["] = "@class.outer",
-				},
-				goto_previous_start = {
-					["[a"] = "@function.outer",
-					["[["] = "@class.outer",
-				},
-				goto_previous_end = {
-					["[A"] = "@function.outer",
-					["[]"] = "@class.outer",
-				},
-			},
-
-			swap = {
-				enable = true,
-				swap_next = {
-					["<leader>a"] = "@parameter.inner",
-				},
-				swap_previous = {
-					["<leader>A"] = "@parameter.inner",
-				},
-			},
-		},
-	})
-end
+-- if vim.fn.has("win32") ~= 1 then
+-- 	require("nvim-treesitter.configs").setup({
+-- 		ensure_installed = { "c", "cpp", "json", "javascript", "go", "python", "rust", "query", "lua" },
+-- 		highlight = {
+-- 			enable = true,
+-- 		},
+-- 		incremental_selection = {
+-- 			enable = true,
+-- 			keymaps = {
+-- 				-- init_selection = "gnn",
+-- 				-- node_incremental = "grn",
+-- 				-- scope_incremental = "rc",
+-- 				-- node_decremental = "grm",
+-- 				init_selection = "`",
+-- 				node_incremental = "`",
+-- 				node_decremental = "~",
+-- 				scope_incremental = "rc",
+-- 			},
+-- 		},
+-- 		textobjects = {
+-- 			select = {
+-- 				enable = true,
+-- 				keymaps = {
+-- 					-- You can use the capture groups defined in textobjects.scm
+-- 					["af"] = "@function.outer",
+-- 					["if"] = "@function.inner",
+-- 					["ac"] = "@class.outer",
+-- 					["ic"] = "@class.inner",
+-- 
+-- 					-- Or you can define your own textobjects like this
+-- 					["iF"] = {
+-- 						python = "(function_definition) @function",
+-- 						cpp = "(function_definition) @function",
+-- 						c = "(function_definition) @function",
+-- 						java = "(method_declaration) @function",
+-- 					},
+-- 				},
+-- 			},
+-- 			move = {
+-- 				enable = true,
+-- 				goto_next_start = {
+-- 					["]a"] = "@function.outer",
+-- 					["]]"] = "@class.outer",
+-- 				},
+-- 				goto_next_end = {
+-- 					["]A"] = "@function.outer",
+-- 					["]["] = "@class.outer",
+-- 				},
+-- 				goto_previous_start = {
+-- 					["[a"] = "@function.outer",
+-- 					["[["] = "@class.outer",
+-- 				},
+-- 				goto_previous_end = {
+-- 					["[A"] = "@function.outer",
+-- 					["[]"] = "@class.outer",
+-- 				},
+-- 			},
+-- 
+-- 			swap = {
+-- 				enable = true,
+-- 				swap_next = {
+-- 					["<leader>a"] = "@parameter.inner",
+-- 				},
+-- 				swap_previous = {
+-- 					["<leader>A"] = "@parameter.inner",
+-- 				},
+-- 			},
+-- 		},
+-- 	})
+-- end
 
 vim.cmd([[
   augroup packer_user_config
