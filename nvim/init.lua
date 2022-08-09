0398a71130d7fbc2c28b9decae010d3119034b24
Jakub Arnold <darthdeus@gmail.com>
2022-08-09 02:28:57 +0200

    Disable vsnip s/S mapping

--- nvim/init.lua
+++ nvim/init.lua
@@ -305,10 +305,6 @@ smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-T
 
 " Select or cut text to use as $TM_SELECTED_TEXT in the next snippet.
 " See https://github.com/hrsh7th/vim-vsnip/pull/50
-nmap        s   <Plug>(vsnip-select-text)
-xmap        s   <Plug>(vsnip-select-text)
-nmap        S   <Plug>(vsnip-cut-text)
-xmap        S   <Plug>(vsnip-cut-text)
 ]], true)
 
 
