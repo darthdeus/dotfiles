aec6917fd9fafd0cf9de5d4c0ebdecb97a944c38
Jakub Arnold <darthdeus@gmail.com>
2022-10-16 03:56:32 +0200

    Fix deprecated update_capabilities

--- nvim/init.lua
+++ nvim/init.lua
@@ -724,7 +724,7 @@ cmp.setup({
 -- vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
 
 -- Setup lspconfig.
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require("lspconfig")["rust_analyzer"].setup({
 	capabilities = capabilities,
