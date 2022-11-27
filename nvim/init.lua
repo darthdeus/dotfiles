9a40b8ad9810256a12b27e0ee32e068f72a4cf40
Jakub Arnold <darthdeus@gmail.com>
2022-11-27 19:24:28 +0100

    Set zshrc.dot as zsh filetype

--- nvim/init.lua
+++ nvim/init.lua
@@ -601,6 +601,7 @@ au BufRead,BufNewFile */funcs/* setfiletype zsh
 " when you enter a (new) buffer
 augroup set-commentstring-ag
 autocmd!
+autocmd BufEnter .zshrc.dot set ft=zsh
 autocmd BufEnter *.wgsl,*.glsl,*.vert,*.frag :lua vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
 " when you've changed the name of a file opened in a buffer, the file type may have changed
 autocmd BufFilePost *.wgsl,*.glsl,*.vert,*.frag :lua vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
