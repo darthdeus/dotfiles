f38e8d63712e6a57753a5afb7e14607fd1ebef42
Jakub Arnold <darthdeus@gmail.com>
2023-07-01 04:04:37 +0200

    Add todo-comments.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -115,6 +115,16 @@ require("lazy").setup({
 	--   }
 	-- },
 
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+        require("todo-comments").setup({
+            signs = false,
+        })
+		end,
+	},
+
 	"jaawerth/fennel.vim",
 	"RRethy/vim-illuminate",
 
@@ -356,6 +366,14 @@ require("neodev").setup({
 })
 
 require("dapui").setup()
+
+vim.keymap.set("n", "]t", function()
+	require("todo-comments").jump_next()
+end, { desc = "Next todo comment" })
+
+vim.keymap.set("n", "[t", function()
+	require("todo-comments").jump_prev()
+end, { desc = "Previous todo comment" })
 
 -- vim.cmd('colorscheme base16-gruvbox-dark-soft')
 -- vim.cmd('colorscheme base16-default-dark')
