d8e48530d0097707a19ae8e8f936f0d464603153
Jakub Arnold <darthdeus@gmail.com>
2023-08-29 02:04:46 +0200

    Add rebel treesitter back

--- nvim/init.lua
+++ nvim/init.lua
@@ -594,17 +594,17 @@ command! Ner :NvimTreeToggle
 local parsers = require("nvim-treesitter.parsers")
 local parser_config = parsers.get_parser_configs()
 
--- parser_config.rebel = {
--- 	install_info = {
--- 		url = "~/projects/tree-sitter-rebel", -- local path or git repo
--- 		files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
--- 		-- optional entries:
--- 		branch = "master", -- default branch in case of git repo if different from master
--- 		generate_requires_npm = true, -- if stand-alone parser without npm dependencies
--- 		requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
--- 	},
--- 	filetype = "rbl", -- if filetype does not match the parser name
--- }
+parser_config.rebel = {
+	install_info = {
+		url = "~/projects/tree-sitter-rebel", -- local path or git repo
+		files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+		-- optional entries:
+		branch = "master", -- default branch in case of git repo if different from master
+		generate_requires_npm = true, -- if stand-alone parser without npm dependencies
+		requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
+	},
+	filetype = "rbl", -- if filetype does not match the parser name
+}
 
 require("nvim-treesitter.configs").setup({
 	ensure_installed = {
