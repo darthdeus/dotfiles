ec6c100dddecac2bcc4cc38c5ac519cd4d03ef57
Jakub Arnold <darthdeus@gmail.com>
2023-08-29 02:04:31 +0200

    Add neorg

--- nvim/init.lua
+++ nvim/init.lua
@@ -87,6 +87,27 @@ require("lazy").setup({
 	},
 
 	{
+		"nvim-neorg/neorg",
+		build = ":Neorg sync-parsers",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("neorg").setup({
+				load = {
+					["core.defaults"] = {}, -- Loads default behaviour
+					["core.concealer"] = {}, -- Adds pretty icons to your documents
+					["core.dirman"] = { -- Manages Neorg workspaces
+						config = {
+							workspaces = {
+								notes = "~/notes",
+							},
+						},
+					},
+				},
+			})
+		end,
+	},
+
+	{
 		"nvim-tree/nvim-tree.lua",
 		config = function()
 			require("nvim-tree").setup({
