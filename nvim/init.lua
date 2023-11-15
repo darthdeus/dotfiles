8148bf3124b30abd320398a33c71cdb3e40e315b
Jakub Arnold <darthdeus@gmail.com>
2023-11-15 01:24:00 +0100

    Fix bashls config

--- nvim/init.lua
+++ nvim/init.lua
@@ -592,7 +592,7 @@ require("mason").setup()
 require("mason-lspconfig").setup {
   ensure_installed = {
     "rust_analyzer",
-    "bash-language-server",
+    "bashls",
     "jsonls",
     -- "shellcheck",
     "taplo",
