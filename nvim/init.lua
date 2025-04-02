53bf72577f466f7e2fa8c5459fb313e47a402932
Jakub Arnold <darthdeus@gmail.com>
2025-04-02 12:11:39 +0200

    Add nvim-lsp-endhints

--- nvim/init.lua
+++ nvim/init.lua
@@ -283,11 +283,11 @@ require("lazy").setup {
     lazy = false, -- This plugin is already lazy
   },
 
-  -- {
-  --   "chrisgrieser/nvim-lsp-endhints",
-  --   event = "LspAttach",
-  --   opts = {}, -- required, even if empty
-  -- },
+  {
+    "chrisgrieser/nvim-lsp-endhints",
+    event = "LspAttach",
+    opts = {}, -- required, even if empty
+  },
 
   "chrisbra/vim-zsh",
   "habamax/vim-godot",
@@ -452,25 +452,24 @@ require "user.reload"
 
 vim.lsp.inlay_hint.enable(true)
 
--- default settings
--- require("lsp-endhints").setup {
--- 	icons = {
--- 		type = "=> ",
--- 		parameter = "󰏪 ",
--- 		offspec = " ", -- hint kind not defined in official LSP spec
--- 		unknown = " ", -- hint kind is nil
--- 	},
--- 	label = {
--- 		truncateAtChars = 20,
--- 		padding = 1,
--- 		marginLeft = 0,
--- 		sameKindSeparator = ", ",
--- 	},
--- 	extmark = {
--- 		priority = 50,
--- 	},
--- 	autoEnableHints = true,
--- }
+require("lsp-endhints").setup {
+	icons = {
+		type = "=> ",
+		parameter = "󰏪 ",
+		offspec = " ", -- hint kind not defined in official LSP spec
+		unknown = " ", -- hint kind is nil
+	},
+	label = {
+		truncateAtChars = 20,
+		padding = 1,
+		marginLeft = 0,
+		sameKindSeparator = ", ",
+	},
+	extmark = {
+		priority = 50,
+	},
+	autoEnableHints = true,
+}
 
 local auto_session = require "auto-session"
 
