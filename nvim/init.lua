a4d6ed03133cda3bf603e80f5d900152d68332a5
Jakub Arnold <darthdeus@gmail.com>
2025-04-20 22:40:31 +0200

    Lower debounce for copilot, for now

--- nvim/init.lua
+++ nvim/init.lua
@@ -464,7 +464,7 @@ require("lazy").setup {
     "zbirenbaum/copilot.lua",
     config = function()
       require("copilot").setup {
-        suggestion = { enabled = false, auto_trigger = false, },
+        suggestion = { enabled = false, auto_trigger = false, debounce = 20 },
         panel = { enabled = false },
       }
     end,
